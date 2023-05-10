14eb2c811085a0128dfdd9b94ab6868d2ce2c86c
Shiraz <shirazsorijoun@gmail.com>
2023-05-10 14:30:45 +0300

    BoardStatus update

--- src/test/BoardStatus.java
+++ src/test/BoardStatus.java
@@ -1,8 +1,24 @@
 package test;
-
-public class BoardStatus {
-
+import java.io.Serializable;
 
 
+public class BoardStatus implements Serializable{
 
+    private static BoardStatus _instance = null;
+    Board board;
+    int score;
+
+
+    public BoardStatus(){
+        board = Board.getBoard();
+        this.score = 0;
+    }
+
+    public static BoardStatus getBoardStatus( ) {
+        if(_instance == null)
+        {
+            _instance = new BoardStatus();
+        }
+        return _instance;
+    }
 }
