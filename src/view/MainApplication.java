06a65785e29d289086d9f87d68e5b544a7dd7c58
Shiraz <shirazsorijoun@gmail.com>
2023-06-06 11:16:52 +0300

    last update

--- src/view/MainApplication.java
+++ src/view/MainApplication.java
@@ -7,6 +7,8 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import viewModel.MainViewModel;
 
+import java.util.Scanner;
+
 public class MainApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -15,7 +17,7 @@ public class MainApplication extends Application {
         VBox root = loader.load();
 
         MainViewController controller = loader.getController();
-        MainViewModel viewModel = new MainViewModel();
+        MainViewModel viewModel = new MainViewModel(); // צריך ליצור מופע של VIEWMODEL ולהכניס את הנתונים שקיבלנו תחילה מה VIEW
 
         controller.setViewModel(viewModel);
 
@@ -24,6 +26,53 @@ public class MainApplication extends Application {
         primaryStage.show();
     }
 
+
+    public void start123(){
+        boolean ishost;
+        boolean islocal;
+        String num = "null";
+        String ip = "null";
+        String port = "null";
+        String playerType;
+        String gameType;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String name = input.nextLine();
+
+        System.out.println("What type of player are you? Enter 1 for Host, Enter 2 for Guest");
+        playerType = input.nextLine();
+
+        if (playerType.equals("1")){ // HOST
+            ishost = true;
+            System.out.println("you are now in Host mode");
+            System.out.println("Do you want to play a local game or host a remote game? for local enter: 1, for remote enter: 2");
+            gameType = input.nextLine();
+            System.out.println("How many player including you are playing? 1-4");
+            num = input.nextLine();
+
+            if(gameType.equals("1")) // LOCAL
+            {
+                islocal = true;
+
+            }
+            else { // REMOTE
+                islocal = false;
+            }
+        }
+        else { // GUEST
+            ishost = false;
+            islocal = false;
+            System.out.println("you are now in Guest mode");
+            System.out.println("Please enter server ip");
+            ip = input.nextLine();
+            System.out.println("Please enter server port");
+            port = input.nextLine();
+
+        }
+        // TODO - create the viewmodel object after a startgame button was pressed
+        this.viewModel = new MainViewModel(name, ip, port, ishost, islocal, Integer.parseInt(num));
+    }
     public static void main(String[] args) throws Exception {
         launch(args);
     }
